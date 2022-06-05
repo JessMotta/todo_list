041c5d85ce7d76240dbc19557b21f381e32506ec
JessMotta <jessicalimamotta@gmail.com>
2022-06-05 16:51:23 -0300

    add modal

---
+++ src/components/ListItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from "./Card";
+
+export default function ListItem(props) {
+  function DoneImg(props) {
+    if (props.done) {
+      return <img alt="feito" src="./assets/on.png"></img>;
+    } else {
+      return <img alt="pendente" src="./assets/off.png"></img>;
+    }
+  }
+
+  return (
+         <li>
+          <Card className={props.item.done ? "done item" : "item"}>
+            {props.item.text}
+            <div>
+              <button
+                onClick={() => {
+                  props.onDone(props.item);
+                }}
+              >
+                <DoneImg done={props.item.done}></DoneImg>
+              </button>
+              <button
+                onClick={() => {
+                  props.onItemDeleted(props.item);
+                }}
+              >
+                <img alt="deletar item" src="./assets/bin.png"></img>
+              </button>
+            </div>
+          </Card>
+        </li>
+
+  );
+}
---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+export default function Modal(props) {
+  return (
+    <div className='modal'>
+        <Card className="cardModal">
+            teste
+        </Card>
+    </div>
+  )
+}
